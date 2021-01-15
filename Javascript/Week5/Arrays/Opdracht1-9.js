ab5898b95ec8e7dd7a3abfa4741594d5dff9a881
Vanessa <v.braumuller@gmail.com>
2021-01-15 17:06:10 +0100

    JS Scopes & Arrays

---
+++ Javascript/Week5/Arrays/Opdracht1-9.js
@@ -0,0 +1,30 @@
+// let numbers = [10, 2, 5, 8, 3, 1, 6, 9, 7, 4];
+// console.log(numbers);
+
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// console.log(fruit);
+
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// console.log(fruit[0]);
+
+let numbers = [10, 2, 5, 8, 3, 1, 6, 9, 7, 4];
+let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+console.log(fruit[3]);
+
+// const randomName = () => {
+//     const namen = [
+//         "John",
+//         "Olly",
+//         "Mo",
+//         "Hendrik",
+//         "Tony",
+//         "Irus",
+//         "Jan",
+//         "Peter",
+//     ];
+
+//     const randomNaam = namen[Math.round(Math.random() * 8)];
+//     console.log(randomNaam);
+//     checkedName.innerHTML = "Dit is " + randomNaam + " en " +randomNaam + " is een bekende van ons";
+
+

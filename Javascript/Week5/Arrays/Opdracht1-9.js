19483906222eb163b38d8c23d9dcd4b115e12cc1
Vanessa <v.braumuller@gmail.com>
2021-01-16 21:46:34 +0100

    JS- Arrays
    Opdracht 1 t/ m 9

--- Javascript/Week5/Arrays/Opdracht1-9.js
+++ Javascript/Week5/Arrays/Opdracht1-9.js
@@ -7,24 +7,50 @@
 // let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
 // console.log(fruit[0]);
 
-let numbers = [10, 2, 5, 8, 3, 1, 6, 9, 7, 4];
+// let numbers = [10, 2, 5, 8, 3, 1, 6, 9, 7, 4];
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// console.log(fruit[3]);
+
+// const random = numbers[Math.round(Mathrandom() *10)];
+
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// let groenten = fruit.push("brocolli", "spitskool");
+// console.log(groenten);
+
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// let laatsteElement = fruit.pop();
+// console.log(laatsteElement);
+
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// let eersteElement = fruit.pop();
+// console.log(eersteElement);
+
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// let citrus = fruit.slice(2, 4);
+// console.log(citrus);
+
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// let updateFruit = fruit.splice(2, 1, "Aardbeien");
+// console.log(updateFruit);
+
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// fruit.sort();
+// console.log(fruit);
+
+// let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
+// let lengte = fruit.length;
+// console.log(fruit);
+
 let fruit = ["Appel", "Banaan", "Kiwi", "Manderijn", "Watermeloen"];
-console.log(fruit[3]);
+console.log(fruit);
+fruit[2] = "Aardbeien";
+console.log(fruit);
 
-// const randomName = () => {
-//     const namen = [
-//         "John",
-//         "Olly",
-//         "Mo",
-//         "Hendrik",
-//         "Tony",
-//         "Irus",
-//         "Jan",
-//         "Peter",
-//     ];
 
-//     const randomNaam = namen[Math.round(Math.random() * 8)];
-//     console.log(randomNaam);
-//     checkedName.innerHTML = "Dit is " + randomNaam + " en " +randomNaam + " is een bekende van ons";
+
+
+
+
+
 
 

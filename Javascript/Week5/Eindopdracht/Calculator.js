4ad9fb9517825970ac29cf960df8689da6167908
Vanessa <v.braumuller@gmail.com>
2021-01-25 16:55:59 +0100

    Eindopdracht-JS
    Calculator javascript

---
+++ Javascript/Week5/Eindopdracht/Calculator.js
@@ -0,0 +1,44 @@
+
+
+    
+   function numeriek() {
+    const cijfer1 = document.getElementById("but[1]").innerHTML;
+    const cijfer2 = document.getElementById("but[2]").innerHTML;
+    const cijfer3 = document.getElementById("but[3]").innerHTML;
+    const cijfer4 = document.getElementById("but[4]").innerHTML;
+    const cijfer5 = document.getElementById("but[5]").innerHTML;
+    const cijfer6 = document.getElementById("but[6]").innerHTML;
+    const cijfer7 = document.getElementById("but[7]").innerHTML;
+    const cijfer8 = document.getElementById("but[8]").innerHTML;
+    const cijfer9 = document.getElementById("but[9]").innerHTML;
+
+
+    const keersom = document.getElementById("decimal").innerHTML;
+    const optellen = document.getElementById("plus").innerHTML;
+    const aftrekken = document.getElementById("minus").innerHTML;
+    const delen = document.getElementById("slash").innerHTML;
+
+    const display = document.getElementById("displayOutput").innerHTML;
+
+
+    console.log(cijfer1);
+
+   
+    
+   }
+numeriek();
+
+
+
+
+//    function functies() {
+//     const keersom = document.getElementById("decimal").innerHTML;
+//     const optellen = document.getElementById("plus").innerHTML;
+//     const aftrekken = document.getElementById("minus").innerHTML;
+//     const delen = document.getElementById("slash").innerHTML;
+//    }
+  
+
+
+
+

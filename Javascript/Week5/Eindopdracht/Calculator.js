781b41fdf463e8e157b3fe7bc96d5286a4c4ddcf
Vanessa <v.braumuller@gmail.com>
2021-01-27 13:47:29 +0100

    JS-Eindopdracht
    Calculator-nog niet af :-(

--- Javascript/Week5/Eindopdracht/Calculator.js
+++ Javascript/Week5/Eindopdracht/Calculator.js
@@ -1,82 +1,185 @@
 
 
-    
-   function numeriek() {
-    const cijfer1 = document.getElementById("but[1]").innerHTML;
-    const cijfer2 = document.getElementById("but[2]").innerHTML;
-    const cijfer3 = document.getElementById("but[3]").innerHTML;
-    const cijfer4 = document.getElementById("but[4]").innerHTML;
-    const cijfer5 = document.getElementById("but[5]").innerHTML;
-    const cijfer6 = document.getElementById("but[6]").innerHTML;
-    const cijfer7 = document.getElementById("but[7]").innerHTML;
-    const cijfer8 = document.getElementById("but[8]").innerHTML;
-    const cijfer9 = document.getElementById("but[9]").innerHTML;
+
+function nummerinvoeren0() {
+   let cijfer0 = document.getElementById("zero").innerHTML;
+   let display = document.getElementById("displayOutput").innerHTML;
+
+   if (cijfer0) {
 
 
-    const keersom = document.getElementById("multiplyl").innerHTML;
-    const optellen = document.getElementById("plus").innerHTML;
-    const aftrekken = document.getElementById("minus").innerHTML;
-    const delen = document.getElementById("divide").innerHTML;
-
-    const display = document.getElementById("displayOutput").innerHTML;
+      document.getElementById("displayOutput").innerHTML += cijfer0;
 
 
-    console.log(cijfer1);
-    console.log(display);
 
-    
    }
-numeriek();
+   // console.log(cijfer1);
+   // console.log(display);
 
-// const rekenmachine = document.querySelector('.calculator');
-// const knoppen = rekenmachine.querySelector('.numbers');
+}
 
-// knoppen.addEventListener('click', e => {
-//    if(e.target.matches('button')) {
-
-//    }
-// })
-
-// const knoppen = e.target;
-// const action = knoppen.CDATA_SECTION_NODE.action;
-
-// if(!action) {
-//    console.log('numer knoppen');
-// }
-
-// if (
-//    action === 'add' ||
-//    action === 'substract' ||
-//    action === 'multiply' ||
-//    action === 'divide' ||
-// )
-//    {
-//    console.log()
-// }
-
-// const calculator = document.querySelector(‘.calculator’)
-// const keys = calculator.querySelector(‘.calculator__keys’)
-
-// keys.addEventListener(‘click’, e => {
-//  if (e.target.matches(‘button’)) {
-//    // Do something
-//  }
-// })
+nummerinvoeren0();
 
 
+function nummerinvoeren1() {
+   let cijfer1 = document.getElementById("but[1]").innerHTML;
+   let display = document.getElementById("displayOutput").innerHTML;
+
+   if (cijfer1) {
+
+
+      document.getElementById("displayOutput").innerHTML += cijfer1;
+
+   }
+   // console.log(cijfer1);
+   // console.log(display);
+
+}
+
+nummerinvoeren1();
+
+
+function nummerinvoeren2() {
+
+   let cijfer2 = document.getElementById("but[2]").innerHTML;
+
+
+   if (cijfer2) {
+      document.getElementById("displayOutput").innerHTML += cijfer2;
+   }
+   // console.log(cijfer2);
+}
+nummerinvoeren2();
+
+
+function nummerinvoeren3() {
+
+   let cijfer3 = document.getElementById("but[3]").innerHTML;
+
+
+   if (cijfer3) {
+      document.getElementById("displayOutput").innerHTML += cijfer3;
+   }
+   // console.log(cijfer2);
+}
+nummerinvoeren4();
+
+function nummerinvoeren4() {
+
+   let cijfer4 = document.getElementById("but[4]").innerHTML;
+
+
+   if (cijfer4) {
+      document.getElementById("displayOutput").innerHTML += cijfer4;
+   }
+   // console.log(cijfer2);
+}
+nummerinvoeren5();
+
+function nummerinvoeren5() {
+
+   let cijfer5 = document.getElementById("but[5]").innerHTML;
+
+
+   if (cijfer5) {
+      document.getElementById("displayOutput").innerHTML += cijfer5;
+   }
+   // console.log(cijfer2);
+}
+nummerinvoeren5();
+
+function nummerinvoeren6() {
+
+   let cijfer6 = document.getElementById("but[6]").innerHTML;
+
+
+   if (cijfer6) {
+      document.getElementById("displayOutput").innerHTML += cijfer6;
+   }
+   // console.log(cijfer2);
+}
+nummerinvoeren6();
+
+function nummerinvoeren7() {
+
+   let cijfer7 = document.getElementById("but[7]").innerHTML;
+
+
+   if (cijfer7) {
+      document.getElementById("displayOutput").innerHTML += cijfer7;
+   }
+   // console.log(cijfer2);
+}
+nummerinvoeren7();
+
+function nummerinvoeren8() {
+
+   let cijfer8 = document.getElementById("but[8]").innerHTML;
+
+
+   if (cijfer8) {
+      document.getElementById("displayOutput").innerHTML += cijfer8;
+   }
+   // console.log(cijfer2);
+}
+nummerinvoeren8();
+
+function nummerinvoeren9() {
+
+   let cijfer9 = document.getElementById("but[9]").innerHTML;
+
+
+   if (cijfer9) {
+      document.getElementById("displayOutput").innerHTML += cijfer9;
+   }
+   // console.log(cijfer2);
+}
+nummerinvoeren9();
+
+function nummerinvoerenpunt() {
+
+   let punt = document.getElementById("punt").innerHTML;
+
+
+   if (punt) {
+      document.getElementById("displayOutput").innerHTML = punt;
+   }
+   // console.log(cijfer2);
+}
+nummerinvoerenpunt();
+
+function operatorinvoeren() {
+   const operator = document.getElementsByClassName("btn_operator");
+
+   console.log(operator);
+}
+
+operatorinvoeren();
 
 
 
 
 
-//    function functies() {
-//     const keersom = document.getElementById("decimal").innerHTML;
-//     const optellen = document.getElementById("plus").innerHTML;
-//     const aftrekken = document.getElementById("minus").innerHTML;
-//     const delen = document.getElementById("slash").innerHTML;
-//    }
-  
+// function nummerinvoeren() {
+//    const cijfer0 = document.getElementById("zero").innerHTML;
+//    const cijfer1 = document.getElementById("but[1]").innerHTML;
+//    const cijfer2 = document.getElementById("but[2]").innerHTML;
+//    const cijfer3 = document.getElementById("but[3]").innerHTML;
+//    const cijfer4 = document.getElementById("but[4]").innerHTML;
+//    const cijfer5 = document.getElementById("but[5]").innerHTML;
+//    const cijfer6 = document.getElementById("but[6]").innerHTML;
+//    const cijfer7 = document.getElementById("but[7]").innerHTML;
+//    const cijfer8 = document.getElementById("but[8]").innerHTML;
+//    const cijfer9 = document.getElementById("but[9]").innerHTML;
 
 
+//    const display = document.getElementById("displayOutput").innerHTML;
+
+//    const keersom = document.getElementById("multiplyl").innerHTML;
+//    const optellen = document.getElementById("plus").innerHTML;
+//    const aftrekken = document.getElementById("minus").innerHTML;
+//    const delen = document.getElementById("divide").innerHTML;
+//    const nummerKnop = document.getElementsByClassName("buttnum");
+//    const operator = document.getElementsByClassName("btn_operator");
 
 

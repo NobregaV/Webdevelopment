38a47e25157284a1dc14207b90a2b3465b4e7e04
Vanessa <v.braumuller@gmail.com>
2021-02-12 16:20:16 +0100

    Java extra opdracht
    GELUKT!!

--- ExtraOpdracht/src/com/javak/Main.java
+++ ExtraOpdracht/src/com/javak/Main.java
@@ -3,9 +3,11 @@ package com.javak;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.Scanner;
+import static java.lang.Integer.parseInt;
 
 public class Main {
 
@@ -13,6 +15,7 @@ public class Main {
 	// write your code here
         Scanner input= new Scanner(System.in);
         Scanner inputDat = new Scanner(System.in);
+
 //        int calculateAge;
 //        Comparator<Date> calculateAge = (Date birthDate, Date currentDate) ->
 //        {
@@ -28,13 +31,34 @@ public class Main {
         System.out.println ("Wat is uw naam?");
 
         String invulNaam = input.nextLine();
-        System.out.println("------------");
-        System.out.println("Wat is uw geboortedatum? Vul in als volgt: dd/mm/jjjj ");
+
+        System.out.println("Wat is uw geboortedatum?");
+        System.out.println("Vul deze in als volgt: dd/mm/jjjj");
         String invulDatum = inputDat.nextLine();
-        System.out.println(invulDatum);
+//        System.out.println(invulDatum);
 
         String[] slashWeg = invulDatum.split("/");
-      
+
+        int dag = parseInt(slashWeg[0]);
+        int mnd = parseInt(slashWeg[1]);
+        int jaar = parseInt(slashWeg[2]);
+//        System.out.println(dag);
+//        System.out.println(mnd);
+//        System.out.println(jaar);
+
+        LocalDate vandaag = LocalDate.now();
+        LocalDate gebdat = LocalDate.of(jaar, mnd, dag);
+//        System.out.println(gebdat);
+//        System.out.println(vandaag);
+
+        Period leeftijd = Period.between(gebdat, vandaag);
+        System.out.println("Hallo " + invulNaam + " uw leeftijd is " + leeftijd.getYears() + " jaar.");
+        System.out.println("Fijne dag verder!");
+
+
+
+
+
 
 
 

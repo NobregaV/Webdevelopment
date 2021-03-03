6f4e94943c7638139c5354150074a4cf001a08c9
Vanessa <v.braumuller@gmail.com>
2021-03-03 17:11:32 +0100

    Test Regex in main

--- Eindopdracht/src/com/javak/Main.java
+++ Eindopdracht/src/com/javak/Main.java
@@ -1,10 +1,17 @@
 package com.javak;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+Pattern wachtWoord = Pattern.compile("/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,}$/");
+        Matcher ww = wachtWoord.matcher();
+         Boolean w = ww.matches();
 
+         
     }
 }
